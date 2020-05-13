93780652853da965463b976484601b704621f37b
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-05-12 21:08:58 -0600

    created about page

---
+++ src/pages/about.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import BackgroundSection from '../components/global/BackgroundSection'
+import Info from '../components/Home/info'
+
+const AboutPage = ({data}) => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <BackgroundSection 
+    img={data.img.childImageSharp.fluid} title="About Us" styleClass="about-background" 
+    />
+    <Info />
+  </Layout>
+)
+
+export const query = graphql`
+{
+  img: file(relativePath: {eq: "about-background.jpeg"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}`
+
+export default AboutPage;
