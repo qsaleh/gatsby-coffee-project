224ec46c28ffff4dcfe7c4ea51b1d6ba2f41ea7c
Qusai Saleh <qusaisaleh@Candices-MacBook-Pro.local>
2020-05-12 23:14:44 -0600

    Connected to contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,9 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
+
   siteMetadata: {
     title: `Regular Joe's`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -11,6 +16,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-transformer-sharp`,
